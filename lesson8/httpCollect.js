7977049679cb3573410144028f612a602f4edd99
Thomas Taylor <tom@taylortom.co.uk>
2014-10-05 18:21:03 +0100

    Add lesson 8

---
+++ lesson8/httpCollect.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+http.get(process.argv[2], function(response){
+    var fullResponse = "";
+    response.setEncoding("utf8");
+
+    response.on("data", function(data){
+        fullResponse += data;
+    });
+
+    response.on("end", function(data){
+        console.log(fullResponse.length)
+        console.log(fullResponse)
+    });
+
+    response.on("error", console.error);
+});
